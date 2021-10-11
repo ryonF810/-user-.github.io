1005af0c7dd42d435738fb72ed26aef2e49a9a94
ryonF810 <91443730+ryonF810@users.noreply.github.com>
2021-10-11 00:56:56 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -2,3 +2,33 @@ function hello(){
 alert ("Hello world!");
 document.getElementById("big").style.fontSize = "4em";
 }
+function change(){
+
+    document.getElementById("big").style.fontStyle = "italic";
+}
+
+function changeBack(){
+
+    document.getElementById("big").style.fontStyle = "normal";
+}
+
+function popsUp(){
+    alert("changed the value");
+    document.getElementById("big").style.fontWeight = "bold";
+    document.getElementById("big").style.color = "blue";
+    document.getElementById("big").style.textDecoration = "underline";
+
+}
+
+function normalFont(){
+    document.getElementById("big").style.fontWeight = "normal";
+
+}
+function uppercase(){
+   var str = document.getElementById("big").value;
+  var upper = str.toUpperCase();
+  document.getElementById("big").innerHTML = upper;
+  var parts = str.split(".");
+  str = parts.join("-Moo");
+   
+}
